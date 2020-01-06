7ddf15581bfa32f83128040e588357043aeec628
shineby <shineby@gmail.com>
2020-01-07 02:15:46 +0900

    aa

--- list_codes.cpp
+++ list_codes.cpp
@@ -20,14 +20,13 @@ int main()
 {
     // constructor
     list<string> cache1(1,"b");
-    list<string> cache2 = {"B"};
     print(cache1);
-    print(cache2);    
+    
 
     // 1) push_back, push_front
     cache1.push_back("d");
     cache1.push_back("d");
-    cache1.push_front("a");
+    cache1.push_front("aba");
     cache1.push_front("a");
     print(cache1); //a,a,b,d,d
 
@@ -42,9 +41,10 @@ int main()
     print(cache1); // a,b,d
 
     // !!! find, remove, copy, trnsform !!! ============================
+    // =================================================================
     // 4) find, find_if, insert
     // insert "c2" in front of "d"
-    auto it = find(cache1.begin(), cache2.end(), "d");
+    auto it = find(cache1.begin(), cache1.end(), "d");
     if (it != cache1.end())
         cache1.insert(it,"c2");
     print(cache1);
@@ -56,11 +56,30 @@ int main()
 
     // remove, remove_if
     // remove c1, c2
-    cache1.remove("c1");
-    remove_if(cache1.begin(), cache1.end(), [](string a){return a == "c2";});
+    //cache1.remove("c1");
+    //cache1.remove_if([](string a){ return a == "c1";});
+    it = remove_if(cache1.begin(), cache1.end(), [](string a){return a == "c1";});    
+    cache1.erase(it, cache1.end());
+    print(cache1);
+    it = remove_if(cache1.begin(), cache1.end(), [](string a){return a == "c2";});
+    cache1.erase(it, cache1.end());    
     print(cache1);
 
-    cout << "cache size : " << cache1.size() << endl;
+    //copy, copy_if
+    list<string> cache2(cache1.size(),""); 
+    // 복사하는 대상과 복사하려는 목적 대상의 크기가 같아야한다!!
+    copy(cache1.begin(), cache1.end(), cache2.begin());
+    print(cache2);
+
+    //transform
+    string idata = cache1.front();
+    cout <<  idata.length() << endl;
+    string odata(idata.length(), ' ');
+    transform(idata.begin(), idata.end(), odata.begin(), ::toupper );
+    cout << odata << endl;
+
+
     return 0;
+
 }
 
